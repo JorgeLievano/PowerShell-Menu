90aed1cbf06016644c5ba8ba0cdbff5d8a6ee228
leofrancocalpa <francocalpa@gmail.com>
2020-05-12 16:56:24 -0500

    feat: Get physical memory and swap info

--- Show-Menu.ps1
+++ Show-Menu.ps1
@@ -2,6 +2,7 @@
 Import-Module -Name .\Get-Process-MostUseCPU.psm1
 Import-Module -Name .\Show-Filesystem-Disks.psm1
 Import-Module -name .\Show-Established-Connections.psm1
+Import-Module -Name .\Show-Memory-Swap.psm1
 
 function Show-Menu
 {
@@ -10,7 +11,7 @@ function Show-Menu
     Write-Host "1: Presione '1' Para Obtener los 5 procesos que mas usan tiempo de cpu ."
     Write-Host "2: Presione '2' Para ver los FileSystems actuales."
     Write-Host "3: Presione '3' Para Buscar el archivo mas grande en el filesystem indicado"
-    Write-Host "4: Opcion 4"
+    Write-Host "4: Presione '3' Para ver la cantidad de memoria fisica y swap disponible"
     Write-Host "5: Presione '5' Para ver el numero de conexciones activa y opcionalmente la lista"
     Write-Host "Q: Press 'Q' to quit."
 
@@ -54,7 +55,11 @@ switch ($selection)
         $filesystemPath = Read-Host "Introduzca la ruta del filesystem donde desea buscar"
         Search-High-Lenght-File -path $filesystemPath -numberFiles 1
          break
-     } '5'{
+     } '4' {
+         Get-Memory
+         break
+     }
+     '5'{
          $showList = Read-Host "Presione 's' si desea incluir la lista, cualquier otro si solo desea el numero de conexiones"
          if($showList -eq 's'){
             Show-Established-Connections -ShowList $true
