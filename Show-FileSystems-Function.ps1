0879e59b839ec232aaf404699a9226f28c55881b
Jorge Eliecer Lievano Solis <jorge.lievano@correo.icesi.edu.co>
2020-05-12 10:28:17 -0500

    remove show filesystems script

--- Show-FileSystems-Function.ps1
+++
@@ -1,3 +0,0 @@
-﻿Get-WmiObject win32_logicaldisk  |`
-select  @{n='Nombre';e={$_.Name}},@{n='Nombre de Volumen';e={$_.VolumeName}},`
-@{n="Tamaño (bytes)";e={$_.Size }},@{n="Espacio Libre (bytes)";e={$_.freespace}}|ft -AutoSize
