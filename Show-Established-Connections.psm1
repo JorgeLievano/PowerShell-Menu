cea47b12052f68c648ddbb402362868732d6c1b3
Jorge Eliecer Lievano Solis <jorge.lievano@correo.icesi.edu.co>
2020-05-12 12:48:18 -0500

    Add Show Active Connections Module
    add option for view the number of  established connection and optional show connection list

---
+++ Show-Established-Connections.psm1
@@ -0,0 +1,16 @@
+﻿function Show-Established-Connections{
+
+param(
+[ValidateSet($true,$false)]
+[boolean] $ShowList=$false
+)
+
+$connections= Get-NetTCPConnection -State Established
+$ShowList
+if($ShowList){
+    $connections | ft LocalAddress, LocalPort,RemoteAddress,RemotePort,AppliedSetting 
+}
+
+$connections | measure -Line | fl @{n='Conexiones Activas';e={$_.Lines}}
+}
+Export-ModuleMember -Function Show-Established-Connections
--- Show-Menu.ps1
+++ Show-Menu.ps1
@@ -1,6 +1,7 @@
 ﻿Import-Module -Name .\Search-Bigger-Size-Files.psm1
 Import-Module -Name .\Get-Process-MostUseCPU.psm1
 Import-Module -Name .\Show-Filesystem-Disks.psm1
+Import-Module -name .\Show-Established-Connections.psm1
 
 function Show-Menu
 {
@@ -9,8 +10,8 @@ function Show-Menu
     Write-Host "1: Presione '1' Para Obtener los 5 procesos que mas usan tiempo de cpu ."
     Write-Host "2: Presione '2' Para ver los FileSystems actuales."
     Write-Host "3: Presione '3' Para Buscar el archivo mas grande en el filesystem indicado"
-    Write-Host "3: Opcion 4"
-    Write-Host "3: Opcion 5"
+    Write-Host "4: Opcion 4"
+    Write-Host "5: Presione '5' Para ver el numero de conexciones activa y opcionalmente la lista"
     Write-Host "Q: Press 'Q' to quit."
 
     
@@ -32,7 +33,7 @@ function continue-Menu
 }
 
 function remove-all-OnExit{
-    Remove-Module Get-Process-MostUseCPU,Search-Bigger-Size-Files,Show-Filesystem-Disks
+    Remove-Module Get-Process-MostUseCPU,Search-Bigger-Size-Files,Show-Filesystem-Disks,Show-Established-Connections
 }
 
 
@@ -53,7 +54,16 @@ switch ($selection)
         $filesystemPath = Read-Host "Introduzca la ruta del filesystem donde desea buscar"
         Search-High-Lenght-File -path $filesystemPath -numberFiles 1
          break
-     } 'q' {
+     } '5'{
+         $showList = Read-Host "Presione 's' si desea incluir la lista, cualquier otro si solo desea el numero de conexiones"
+         if($showList -eq 's'){
+            Show-Established-Connections -ShowList $true
+         }else{
+            Show-Established-Connections
+         }
+
+         break
+     }'q' {
          remove-all-OnExit
          return
      }  
